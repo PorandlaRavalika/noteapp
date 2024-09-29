dc468ff8ea3038d60d67e5648b92fc4489a09c28
Porandla Ravalika <117129988+PorandlaRavalika@users.noreply.github.com>
2024-09-29 16:50:23 +0530

    Update server.js

--- notesback/server.js
+++ notesback/server.js
@@ -9,12 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-    origin:["https://noteapp-1whq.vercel.app"],
-    methods:["POST","GET"],
-    credentials:true
-    }
-));
+app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB Atlas
